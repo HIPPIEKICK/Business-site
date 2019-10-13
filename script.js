fed0737418d4465f6bd7c3e3ac03ecd1fd2b3ae2
Matilda Arvidsson <matilda@Matildas-MacBook-Pro.local>
2019-10-13 15:37:58 +0200

    Added javascript to change the booking-button

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
 function toggle() {
   this.classList.toggle("active");
-  // document.getElementById("h2book").innerHTML = toggle("Test");
+  var h2 = document.getElementById("h2");
+  if (h2.innerHTML === "Book now") {
+    h2.innerHTML = "X";
+  } else {
+    h2.innerHTML = "Book now";
+  }
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("button").onclick = toggle;
-
-/*
-const form = getElementById("form");
-const exitbutton = getElementById("exitbutton");
-
-function hideForm() {
-  document.getElementById("form").style.display = "none";
-}
-document.getElementById("exitbutton").onclick = hideForm;
-*/
 
 function toggleFAQ() {
   this.classList.toggle("open");
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggleFAQ;
 document.getElementById("section2").onclick = toggleFAQ;
 document.getElementById("section3").onclick = toggleFAQ;
